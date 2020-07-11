4efe9eddbe671eb46d26cfeabc3d23ab943fe63f
Celeste J Bembry <CelesteJBembry@gmail.com>
2020-07-11 16:49:21 -0500

    basic react layout and added components folder structure

---
+++ client/src/components/MultiChoiceCard/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+//import "./style.css";
+
+function MultiChoiceCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {props.occupation}
+          </li>
+          <li>
+            <strong>Location:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  );
+}
+
+export default MultiChoiceCard;
