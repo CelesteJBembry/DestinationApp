c76e8aa6749a1308bf4d9e9be2868d3e79d601f6
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-07-12 15:35:58 -0500

    working on the api call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import("dotenv").config();
+const triposoUrl = "https://www.triposo.com/api/latest/location.json";
 
 // Export an object containing methods we'll use for accessing the location and events
 export default {
     fetchDestination: function () {
         return axios
-            .get("https://www.triposo.com/api/latest/location.json?id=Houston")
+            .get('${triposoUrl}?id=Houston&account=${[process.env.REACT_APP_ACCOUNT]}&token=${[process.env.REACT.APP.TOKEN]}')
 
             .then(res => {
                 const destination = res.data;
