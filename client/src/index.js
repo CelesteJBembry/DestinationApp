ee7dba54110416ffe85450a116774b35495178fc
lewismcruz <lewis.cruz@gmail.com>
2020-07-15 13:30:08 -0500

    Commiting for merge from stable branch.

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SignIn from './components/signin.js';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- client/src/pages/Destination.js
+++ client/src/pages/Destination.js
@@ -2,35 +2,25 @@ import React, { useEffect, useState } from "react";
 import API from "../utils/API";
 
 
-function Destination() {
-    API.fetchDestination()
-        .then(destination => {
+function Destination(props) {
+    useEffect(() => {
+        API.fetchDestination(props.city)
+            .then(destination => {
+                console.log(destination.data);
+            })
+            .catch(err => console.log(err));
+    }, [])
 
-        })
 
-        .catch(err => console.log(err));
+    return (
+        <p>
+            Hello
+        </p>
+    )
 }
 
 
 
 
-
-
-
-
-
-
-
-return (
-    <div>
-        <h1 className="text-center">Welcome to LinkedUp</h1>
-        <p className="text-center h3">Click on the arrows to browse users</p>
-        <Row>
-            {/* Pass props to the card container */}
-            <CardContainer />
-        </Row>
-    </div>
-);
-}
 
 export default Destination;
