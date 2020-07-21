1591b27ad00ea3e1ad1b34a7bc16741459bd80f4
Celeste J Bembry <CelesteJBembry@gmail.com>
2020-07-21 10:56:51 -0500

    updates to api code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,28 +8,17 @@ export default {
     getQuestions: function () {
         return axios.get("/api/questions");
     },
+    fetchDestination: function (city) {
+        const triposoUrl = "https://www.triposo.com/api/latest/location.json";
+        const queryUrl = `${triposoUrl}?id=${city}&account=${process.env.REACT_APP_ACCOUNT}&token=${process.env.REACT_APP_TOKEN}`;
+        return axios.get(queryUrl);
+    },
 
     fetchTour: function (city) {
 
-        const triposoTourUrl = "https://www.triposo.com/api/20200405/tour.json?";
-        const tourQueryUrl = `${triposoTourUrl}location_ids=${city}&account=${process.env.REACT_APP_ACCOUNT}&token=${process.env.REACT_APP_TOKEN}`;
-
-        return axios
-			//.get(queryUrl)
-		.get(tourQueryUrl)
-
-	}
-};
-// fetchDestination: function (city) {
-//     const triposoUrl = "https://www.triposo.com/api/latest/location.json";
-//     const queryUrl = `${triposoUrl}?id=${city}&account=${process.env.REACT_APP_ACCOUNT}&token=${process.env.REACT_APP_TOKEN}`;
-//     // To check out the format for the city names we need.
-//     // const queryUrl = `${triposoUrl}?annotate=trigram:Ber&account=${process.env.REACT_APP_ACCOUNT}&token=${process.env.REACT_APP_TOKEN}`;
-//     // process of building api call for any tour info.
-//     const triposoTourUrl = "https://www.triposo.com/api/20200405/tour.json?";
-//     const tourQueryUrl = `${triposoTourUrl}?location_ids=${city}&account=${process.env.REACT_APP_ACCOUNT}&token=${process.env.REACT_APP_TOKEN}`;
-//     return axios
-//         .get(queryUrl)
-//     //.get(tourQueryUrl)
-// }
-// };
+       const triposoTourUrl = "https://www.triposo.com/api/20200405/tour.json?";
+       const tourQueryUrl = `${triposoTourUrl}location_ids=${city}&account=${process.env.REACT_APP_ACCOUNT}&token=${process.env.REACT_APP_TOKEN}`;
+       return axios
+           .get(tourQueryUrl)
+   }
+}
